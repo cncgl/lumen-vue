54a833331cb0c14bacee5b23e0d5baacda0c81a9
cncgl <concigel@gmail.com>
2016-08-11 00:08:20 +0900

    add log logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use DB;
+use Log;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    // if (!app()->environment('production')) {
+    if (app()->isLocal() || app()->runningUnitTests()) {
+      DB::listen(function ($query) {
+        Log::debug('sql', [$query->sql, $query->bindings, $query->time]);
+      });
     }
+  }
 }
