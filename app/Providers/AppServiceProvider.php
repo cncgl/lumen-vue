4e999c753737f016af1cc3b75278715d9d36afc7
cncgl <concigel@gmail.com>
2016-09-03 23:57:06 +0900

    drop mail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,10 +17,10 @@ class AppServiceProvider extends ServiceProvider
   public function register()
   {
 
-    $this->app->singleton('mailer', function ($app) {
-      $app->configure('services');
-      return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer');
-    });
+//    $this->app->singleton('mailer', function ($app) {
+//      $app->configure('services');
+//      return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer');
+//    });
 
 
     // if (!app()->environment('production')) {
