acd7994bc7c0ad89b12d09eecf075d3c2e8da76d
cncgl <concigel@gmail.com>
2016-09-19 00:01:17 +0900

    add posts

---
+++ resources/assets/js/pages/post.vue
@@ -0,0 +1,26 @@
+<template lang="jade">
+  .container
+    form(method='post', action='')
+      .form-group.row
+        input.form-control(type='text')
+      .form-group.row
+        textarea.form-control
+</template>
+<style lang="sass">
+  body {
+    background-color: #ff0000;
+  }
+  .container {
+    margin-top: 10px;
+  }
+
+</style>
+<script>
+  export default{
+    data(){
+      return{
+        msg: 'hello vue'
+      }
+    }
+  }
+</script>
