ca2a853f698f8d42ea73e59953466bda27fcac11
cncgl <concigel@gmail.com>
2017-01-30 00:52:45 +0900

    phpcs 対応

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,33 +8,33 @@ use App\Http\Controllers\Controller;
 class ArticleController extends Controller
 {
 
-    public function index()
-    {
-        echo 'test';
-    }
+  public function index()
+  {
+    echo 'test';
+  }
 
-    public function show($id)
-    {
-        //
-    }
+  public function show($id)
+  {
+    //
+  }
 
-    public function create(Request $request)
-    {
-        //
-    }
+  public function create(Request $request)
+  {
+    //
+  }
 
-    public function edit($id)
-    {
-        //
-    }
+  public function edit($id)
+  {
+    //
+  }
 
-    public function update($id, Request $request)
-    {
-        //
-    }
+  public function update($id, Request $request)
+  {
+    //
+  }
 
-    public function destroy($id)
-    {
-        //
-    }
+  public function destroy($id)
+  {
+    //
+  }
 }
